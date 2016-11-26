e93f1ff82a594c5b13702b0e9629a11629651d7f
Jesús Escamilla <jesusescamilla@hotmail.com>
2016-11-26 11:20:43 -0600

    parserStorage class

--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
-module.exports = 'hello';
+
+class parserStorage {
+
+  _json(text) {
+    return JSON.parse(text);
+  }
+
+  _text(json) {
+    return JSON.stringify(json);
+  }
+
+  set(key, data) {
+    window.localStorage.setItem(key, data);
+  }
+
+  get(key) {
+    return window.localStorage.getItem(key);
+  }
+
+  remove(key) {
+    window.localStorage.removeItem(key);
+  }
+
+}
+
+export default new parserStorage();
